ffb300c0f4f3fabe82d03132054aa9103ad266c3
CaritoPava <carito9018@gmail.com>
2022-04-20 18:08:26 -0500

    feat: user registration in process

--- src/components/auth/RegisterView.js
+++ src/components/auth/RegisterView.js
@@ -1,14 +1,52 @@
 import { Link } from 'react-router-dom'
+import validator from 'validator'
+import { useForm } from '../../hooks/useForm'
+import { useDispatch } from 'react-redux'
+import { setError } from '../../actions/ui'
 
 export const RegisterView = () => {
+
+  const dispatch = useDispatch()
+
+  const [formValues, handleInputChange] = useForm({
+    name: 'Carolina',
+    lastname: 'Pava',
+    email: 'carito9018@gmail.com',
+    password: '12345'
+  })
+
+  const { name, lastname, email, password } = formValues
+
+  const handleRegister = (e) => {
+    e.preventDefault()
+    if (isFormValid()) {
+      console.log('Formulario correcto')
+    }
+  }
+  const isFormValid = () => {
+    if (name.trim().length === 0) {
+      dispatch(setError('Name is required'))
+      return false
+    } else if (!validator.isEmail(email)) {
+      dispatch(setError('Email is not valid'))
+      return false
+    } else if (password.length < 5) {
+      dispatch(setError('Password should be at least 6 characteres'))
+      return false
+    }
+
+    return true
+  }
   return (
     <div className='auth__background-register'>
 
       <div className='auth__title'>
         <h1
           className='auth__title-register'
-        >Registro</h1>
-        <form>
+        >Registro
+        </h1>
+        <h2>FORMULARIO INVALIDO</h2>
+        <form onSubmit={handleRegister}>
           <div className='auth__form'>
             <input
               type="text"
@@ -16,12 +54,16 @@ export const RegisterView = () => {
               name='name'
               className='auth__input'
               autoComplete='off'
+              values={name}
+              onChange={handleInputChange}
             />
             <input
               type="text"
               placeholder='Apellido'
               name='lastname'
               className='auth__input'
+              values={lastname}
+              onChange={handleInputChange}
             />
 
             <input
@@ -30,12 +72,16 @@ export const RegisterView = () => {
               name='email'
               className='auth__input'
               autoComplete='off'
+              values={email}
+              onChange={handleInputChange}
             />
             <input
               type="password"
               placeholder='Password'
               name='password'
               className='auth__input'
+              values={password}
+              onChange={handleInputChange}
             />
             <button
               type='submit'
@@ -51,12 +97,7 @@ export const RegisterView = () => {
             </Link>
             <p>O iniciar sesión con</p>
             <div className='auth__form'>
-              <button
-                className='btn-primary btn-apple'
-              >
-                <i className="fa-brands fa-apple mr-5"></i>
-                Con Apple
-              </button>
+
               <div
                 className='auth__redes'>
                 <button
@@ -65,12 +106,7 @@ export const RegisterView = () => {
                   <i className="fa-brands fa-google-plus-g mr-5"></i>
                   Google
                 </button>
-                <button
-                  className='btn-primary btn-mini btn-facebook'
-                >
-                  <i className="fa-brands fa-facebook-f mr-5"></i>
-                  Facebook
-                </button>
+
               </div>
             </div>
           </div>
